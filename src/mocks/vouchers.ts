e778f5c93698b7723cc23128118cb549013a17da
hongsonit10 <hongsonit10@gmail.com>
2025-02-14 10:31:47 +0700

    update: Cập nhật lại mock vouchers, thực tế hơn

--- src/mocks/vouchers.ts
+++ src/mocks/vouchers.ts
@@ -1,6 +1,12 @@
 export const vouchers = [
-  { code: "SALE100K", type: "fixed", value: 100000 },
-  { code: "SALE10%", type: "percent", value: 10 },
-  { code: "SALE500K", type: "fixed", value: 500000 },
-  { code: "SALE20%", type: "percent", value: 20 },
+  { code: "NEWUSER100", type: "fixed", value: 100000 },
+  { code: "FREESHIP50", type: "fixed", value: 50000 },
+  { code: "SUMMER10", type: "percent", value: 10 },
+  { code: "VIP20OFF", type: "percent", value: 20 },
+  { code: "WEEKENDSALE", type: "fixed", value: 200000 },
+  { code: "FLASHSALE30", type: "percent", value: 30 },
+  { code: "LOYALTY50", type: "fixed", value: 50000 },
+  { code: "BIRTHDAYGIFT", type: "fixed", value: 150000 },
+  { code: "BLACKFRIDAY40", type: "percent", value: 40 },
+  { code: "BACKTOSCHOOL", type: "fixed", value: 120000 },
 ];
