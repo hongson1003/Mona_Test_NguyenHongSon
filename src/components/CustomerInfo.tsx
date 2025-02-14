1cda824ce12dbe19e1cbe044a2ae2263c8a89ed3
hongsonit10 <hongsonit10@gmail.com>
2025-02-14 12:58:38 +0700

    mod: modified customerInfo

--- src/components/CustomerInfo.tsx
+++ src/components/CustomerInfo.tsx
@@ -1,57 +1,18 @@
 import { Box, TextField } from "@mui/material";
-import { Controller, useFormContext } from "react-hook-form";
+import { useFormContext } from "react-hook-form";
 
 interface ICustomerInfoProps {
   children?: React.ReactNode;
 }
 
 const CustomerInfo = ({ children }: ICustomerInfoProps) => {
-  const { control } = useFormContext();
+  const { register } = useFormContext();
 
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-      <Controller
-        name="name"
-        control={control}
-        render={({ field }) => (
-          <TextField
-            {...field}
-            label="Tên khách hàng"
-            fullWidth
-            InputLabelProps={{
-              shrink: field.value ? true : undefined,
-            }}
-          />
-        )}
-      />
-      <Controller
-        name="email"
-        control={control}
-        render={({ field }) => (
-          <TextField
-            {...field}
-            label="Email"
-            fullWidth
-            InputLabelProps={{
-              shrink: field.value ? true : undefined,
-            }}
-          />
-        )}
-      />
-      <Controller
-        name="phone"
-        control={control}
-        render={({ field }) => (
-          <TextField
-            {...field}
-            label="Số điện thoại"
-            fullWidth
-            InputLabelProps={{
-              shrink: field.value ? true : undefined,
-            }}
-          />
-        )}
-      />
+      <TextField label="Tên khách hàng" {...register("name")} fullWidth />
+      <TextField label="Email" {...register("email")} fullWidth />
+      <TextField label="Số điện thoại" {...register("phone")} fullWidth />
 
       {children}
     </Box>
